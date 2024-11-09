90ca1707c7861f4d184d69e73ec7c2ff9e86ce3a
paulocerez <paulo.ramirez@code.berlin>
2024-11-09 15:55:18 +0100

    feat: add route to store scraped markdown, extract name from url

---
+++ utils/extractNameFromUrl.ts
@@ -0,0 +1,9 @@
+export default function extractNameFromUrl(url: string) {
+	return url.split(".")[1];
+}
+
+// console.log(extractNameFromUrl("https://www.google.com/"));
+// console.log(extractNameFromUrl("https://developers.notion.com/"));
+// console.log(extractNameFromUrl("https://developers.hubspot.com/beta-docs/reference/api"));
+// console.log(extractNameFromUrl("https://api.qdrant.tech/api-reference"));
+
