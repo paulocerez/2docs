55197d576d7ac7d858ba35c81eba8fdf9c832718
paulocerez <paulo.ramirez@code.berlin>
2024-11-11 23:17:02 +0100

    feat: add db seed script, store plain markdown for testing in json files

---
+++ db/postgres/scripts/insertApiDocs.ts
@@ -0,0 +1,40 @@
+import { db } from "../db";
+import { apiDocumentations, InsertApiDocumentation } from "../schema/api";
+import { faker } from "@faker-js/faker";
+
+async function insertApiDocumentations(count: number) {
+	const batchSize = 100;
+	const totalBatches = Math.ceil(count / batchSize);
+
+	console.log(`Inserting ${count} API documentation records...`);
+
+	for (let i = 0; i < totalBatches; i++) {
+		const batch: InsertApiDocumentation[] = [];
+		for (let j = 0; j < batchSize; j++) {
+			const apiDoc: InsertApiDocumentation = {
+				name: faker.lorem.word(),
+				baseUrl: faker.internet.url(),
+				version: faker.lorem.word(),
+				content: faker.lorem.paragraph(),
+				createdBy: faker.string.uuid(),
+			}
+			batch.push(apiDoc);
+		}
+		await db.insert(apiDocumentations).values(batch);
+		console.log(`Inserted batch ${i + 1} of ${totalBatches}`);
+	}
+	console.log(`Inserted ${count} API documents`);
+}
+
+async function main() {
+	try {
+	  await insertApiDocumentations(2000);
+	  console.log("Insertion complete!");
+	  process.exit(0);
+	} catch (error) {
+	  console.error("Error during insertion:", error);
+	  process.exit(1);
+	}
+  }
+  
+  main();
