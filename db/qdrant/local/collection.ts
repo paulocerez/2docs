b7eccc5e4643e3c4356769ad020edf99ffbe81e6
paulocerez <paulo.ramirez@code.berlin>
2024-11-17 22:20:08 +0100

    remove unnecessary files

--- db/qdrant/local/collection.ts
+++
@@ -1,9 +0,0 @@
-import client from "./local";
-
-export const initializeCollection = async () => {
-    await client.createCollection("test-collection", {
-        vectors: { size: 4, distance: "Dot" },
-    });
-};
-
-initializeCollection();
--- db/qdrant/local/local.ts
+++
@@ -1,5 +0,0 @@
-import { QdrantClient } from "@qdrant/js-client-rest";
-
-const client = new QdrantClient({ host: "localhost", port: 6333 });
-
-export default client;
