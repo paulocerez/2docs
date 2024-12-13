d21c6c18f6f129dde80e429e0386fda9d3cb775e
paulocerez <paulo.ramirez@code.berlin>
2024-12-13 01:48:06 +0100

    feat: add condition in useScrape... hook to check for existing api in db

--- hooks/messages/useScrapeUrlMutation.ts
+++ hooks/messages/useScrapeUrlMutation.ts
@@ -8,21 +8,38 @@ interface ScrapeUrlMutationData {
 
 export function useScrapeUrlMutation() {
 
+	console.log("Scraping url mutation");
   return useMutation({
     mutationFn: async ({ url, userId }: ScrapeUrlMutationData ) => {
-      const response = await fetch('/api/scrape', {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ userId, url }),
-      });
+		// check if api documentation already exists
+		const checkResponse = await fetch('/api/check-documentation', {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ url }),
+		  });
+
+		  if (checkResponse.ok) {
+			const { apiDocId } = await checkResponse.json();
+			if (apiDocId) {
+				console.log("Existing API used for url:", apiDocId);
+				return { apiDocId };
+			}
+		  }
+
+		// if not, scrape the url
+		const scrapeResponse = await fetch('/api/scrape', {
+        	method: "POST",
+        	headers: { "Content-Type": "application/json" },
+        	body: JSON.stringify({ userId, url }),
+	    });
 
 
-      if (!response.ok) {
-		const errorData = await response.json();
+      if (!scrapeResponse.ok) {
+		const errorData = await scrapeResponse.json();
 		throw new Error(errorData.error || "Failed to scrape url");
       }
 
-	  return response.json();
+	  return scrapeResponse.json();
     },
   });
 }
